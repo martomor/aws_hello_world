43fc8641222b686926703af47e6e99d4bfd564df
martomor <martinmoreno313@gmail.com>
2022-04-25 20:05:56 -0500

    including loggers

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
 from fastapi import FastAPI
+import logging
+from logging.config import dictConfig
+from log_config import log_config
 
-app = FastAPI()
+dictConfig(log_config)
 
+logger = logging.getLogger('aws_hello_logger') 
+
+app = FastAPI(info=True)
 
 @app.get('/health')
 def make_request():
+    logger.info("App is ok")
     return 'Hello World!'
